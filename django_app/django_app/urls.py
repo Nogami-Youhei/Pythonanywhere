d8b52d9b9cc727643b1b08ab273bf6d7d73d8071
Nogami-Youhei <nogami.youhei@sekisui.com>
2023-03-29 22:02:57 +0900

    PLSアプリを追加しました

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import hello.views as hello
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("hello/", include('hello.urls')),
+    path('admin/', admin.site.urls),
+    path('pls/', include('pls.urls')),
 ]
