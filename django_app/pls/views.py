81056912c40d0745f3169e95a5b3d8e823708a79
Nogami-Youhei <nogami.youhei@sekisui.com>
2023-03-31 10:09:53 +0900

    主成分の数を入力できるようにしました

--- django_app/pls/views.py
+++ django_app/pls/views.py
@@ -12,21 +12,29 @@ from sklearn.cross_decomposition import PLSRegression
 
 def index(request):
     params = {
-        'range': range(1, 11),
+        'range1': range(1, 11),
         'n': 0,
+        'message': '※説明変数の数を選択してください'
     }
 
-    if (request.POST.get('目的変数')):
-        n = request.POST['n']
+    if request.POST.get('目的変数'):
+        if (int(request.POST['n']) - 1) < int(request.POST['n_components']):
+            params = {
+                'range1': range(1, 11),
+                'n': 0,
+                'message': '説明変数の数より主成分の数が多いです！'
+            }
+
+            return render(request, 'pls/index.html', params)
 
         X = []
         y = np.array(request.POST['目的変数'].split()).astype('float')
 
-        for i in eval(n):
+        for i in eval(request.POST['range2']):
             X.append(np.array(request.POST['説明変数' + str(i)].split()).astype('float'))
 
         PCR_model = make_pipeline(StandardScaler(), 
-                        PCA(n_components=3), 
+                        PCA(n_components=int(request.POST['n_components'])), 
                         LinearRegression())
         
         X = np.array(X).T
@@ -36,24 +44,26 @@ def index(request):
         components = PCR_model.named_steps["pca"].components_
         variance_ratio = PCR_model.named_steps["pca"].explained_variance_ratio_
         predict = PCR_model.predict(X)
-        R2 = round(PCR_model.score(X, y), 2)
+        R2 = round(PCR_model.score(X, y), 4)
 
         params = {
-            'range': range(1, 11),
+            'range1': range(1, 11),
             'n': 0,
-            'components': 'components:<br>' + str(components),
+            'components1': 'components:<br>',
+            'components2': components,
             'variance_ratio': 'variance_ratio:<br>' + str(variance_ratio),
             'predict': 'predict:<br>' + str(predict),
-            'R2': 'R2:<br>' + str(R2),
+            'R2': 'R2: ' + str(R2),
         }
         return render(request, 'pls/index.html', params)
 
     if (request.method == 'POST'):
-        n = range(1, int(request.POST['n']) + 1)
 
         params = {
-            'range': range(1, 11),
-            'n': n,
+            'range1': range(1, 11),
+            'range2': range(1, int(request.POST['n']) + 1),
+            'n': int(request.POST['n']) + 1,
+            'message': '※値を入力してください',
         }
     
     return render(request, 'pls/index.html', params)
